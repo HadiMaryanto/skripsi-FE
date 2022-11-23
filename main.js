1076617615688c4de3fa745c8c0c303790f7a45f
HadiMaryanto <maryantohadi391@gmail.com>
2022-11-23 22:21:04 +0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import 'ol/ol.css';
 import { fromLonLat } from 'ol/proj';
 import { Map, View } from 'ol';
 import { Vector as VectorLayer, Tile as TileLayer } from 'ol/layer';
-import { Vector as VectorSource, Stamen } from 'ol/source';
+import { Vector as VectorSource, Stamen, OSM } from 'ol/source';
+import Overlay from 'ol/Overlay';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 //! [renderer]
@@ -66,7 +67,7 @@ class CustomLayer extends VectorLayer {
             attributes: [{
                     name: 'size',
                     callback: function(feature) {
-                        return 32 * clamp(feature.get('mass') / 200000, 0, 1) + 16;
+                        return 32 * clamp(feature.get('mass') / 200000, 0, 1) + 13;
                     }
                 },
                 {
@@ -152,24 +153,27 @@ const map = new Map({
     target: 'map-container',
     layers: [
         new TileLayer({
-            source: new Stamen({
-                layer: 'toner'
-            })
+            source: new OSM()
         }),
         Custome,
     ],
+    // 101.438309, 0.510440
     view: new View({
-        center: fromLonLat([120.437276, -2.806750]),
+        center: fromLonLat([114.419803, 0.341276]),
         zoom: 5
     })
 });
+// var container = document.getElementById('popup');
+// var content = document.getElementById('popup-content');
+// var closer = document.getElementById('popup-closer');
+
 
 // console.log(map);
 //! [animate]
 const yearElement = document.getElementById('year');
 
 function render() {
-    const elapsed = rate * (Date.now() - start) / 1000;
+    const elapsed = rate * (Date.now() - start) / 6000;
     currentYear = minYear + (elapsed % span);
     yearElement.innerText = currentYear.toFixed(0);
     // console.log(elapsed);
@@ -179,3 +183,79 @@ function render() {
 
 render();
 //! [animate]
+const info = document.getElementById('info');
+info.style.pointerEvents = 'none';
+const tooltip = new bootstrap.Tooltip(info, {
+    animation: false,
+    customClass: 'pe-none',
+    offset: [0, 5],
+    title: '-',
+    trigger: 'manual'
+});
+// info.tooltip({
+//     animation: false,
+//     trigger : 'manual'
+// });
+let currentFeature;
+const displayFeatureInfo = function(pixel, target) {
+    const feature = target.closest('.ol-control')
+    ? undefined
+    : map.forEachFeatureAtPixel(pixel, function (feature){
+        return feature;
+    });
+    if(feature){
+        info.style.left = pixel[0] + 'px';
+        info.style.top = pixel[1] + 'px';
+        if (feature !== currentFeature) {
+            tooltip.setContent({'.tooltip-inner': feature.get('mass')});
+        }
+        if(currentFeature){
+            tooltip.update();
+        }else{
+            tooltip.show();
+        }
+    }else{
+        tooltip.hide();
+    }
+    currentFeature = feature;
+};
+    // info.css({
+    //     left: pixel[0] + 'px',
+    //     top : (pixel[1] - 15) + 'px' 
+    // });
+
+// const feature = map.forEachFeatureAtPixel(pixel, function(feature){
+//     return feature;
+// });
+// if(feature) {
+//     const isi = feature.values_;
+//     const mass = isi.mass;
+
+//     tampil = nama + ' , ';
+
+//     info.tooltip('hide')
+//     .attr('data-original-title', tampil)
+//     .tooltip('fixTitle')
+//     .tooltip('show')
+// }else{
+//     info.tooltip('hide');
+// }
+// };
+
+map.on('pointermove', function(evt){
+    if (evt.dragging) {
+        tooltip.hide();
+        currentFeature = undefined;
+        return;
+    }
+    const pixel = map.getEventPixel(evt.originalEvent);
+    displayFeatureInfo(pixel, evt.originalEvent.target);
+});
+
+map.on('click', function(evt){
+    displayFeatureInfo(evt.pixel, evt.originalEvent.target);
+});
+map.getTargetElement().addEventListener('pointerleave', function () {
+    tooltip.hide();
+    currentFeature = undefined;
+});
